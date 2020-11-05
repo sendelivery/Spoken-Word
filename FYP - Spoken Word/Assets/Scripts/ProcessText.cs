b595f97558fbcb5ed5e28e60fbe45e3765ce3a3e
sendelivery <69419807+sendelivery@users.noreply.github.com>
2020-11-05 18:41:59 +0000

    Initial Project Commit
    Updated gitignore to ignore IBM Watson SDK and teh IBM unity-sdk-core.
    Project now includes an empty scene, with a first person player controller and simple UI elements for speech to text feedback.

---
+++ FYP - Spoken Word/Assets/Scripts/ProcessText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ProcessText : MonoBehaviour
+{
+    public Text textField;
+
+	[SerializeField] private GameObject player;
+
+	private void Start() {
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        if (textField.text.Contains("(Final, ")) {
+
+            player.GetComponent<PlayerMovement>().HandleInput(textField.text);
+            // HandleInput(textField.text);
+		}
+    }
+}
