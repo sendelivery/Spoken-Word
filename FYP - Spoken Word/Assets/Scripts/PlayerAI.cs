45be447d39336776a9b8e44381e983708598d9d5
sendelivery <69419807+sendelivery@users.noreply.github.com>
2020-11-06 15:39:47 +0000

    Added NavMeshAgent
    Added NavMeshAgent and baked navigation mesh into the environment.
    
    Somewhere along the line this caused a bug with jumping and the ground distance used in PlayerMovement.cs isGrounded CheckSphere ln22

---
+++ FYP - Spoken Word/Assets/Scripts/PlayerAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAI : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    [SerializeField] private Waypoint[] waypoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    /*
+    private void HandleObjective() {
+        FaceDirection();
+        if (distanceToTarget >= navMeshAgent.stoppingDistance) {
+            GoToObjective();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+            AttackTarget();
+        }
+    }
+
+    private void GoToObjective() {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void FaceDirection() {
+        Vector3 direction = (target.position - transform.position.normalized);
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+    }
+    */
+}
