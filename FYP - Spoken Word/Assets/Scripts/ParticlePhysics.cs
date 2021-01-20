58da069117dc9bd6c7de8d3c128e5dc5f5adcddd
sendelivery <69419807+sendelivery@users.noreply.github.com>
2021-01-20 14:10:51 +0000

    Thruster, Force & Multiplier
    Added a 'thruster' particle system to launch the ring in ring toss. Also added a multiplier to control the force.

---
+++ FYP - Spoken Word/Assets/Scripts/ParticlePhysics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePhysics : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    public Vector3 force = new Vector3(0.0f, 100.0f, -100.0f);
+    public float multiplier = 1f;
+
+    void OnParticleCollision(GameObject other) {
+        Debug.Log("particle collision");
+        Rigidbody body = GetComponentInChildren<Rigidbody>();
+
+        body.isKinematic = false;
+
+		body.AddForce(force * multiplier);
+    }
+}
